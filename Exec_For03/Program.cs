de3a5c5229aa444d82c545fcd5e7ff245b75fb47
ispan <ispan@DESKTOP-PAKSETB>
2022-10-17 17:28:20 +0800

    加入專案檔案。

---
+++ Exec_For03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For03
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 5; i >= 1; i--)
+			{
+				string row1 = new string('*', i);
+				string row2 = new string(' ', i);
+				Console.WriteLine(row1 +row2);
+			}
+		}
+	}
+}
